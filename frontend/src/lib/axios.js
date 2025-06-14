a0bce611404cb9cfe343dcab3dbb36b906964c02
Priyanshucs22 <priyanshusharma0156@gmail.com>
2025-06-14 17:00:09 +0530

    correct

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-app-backend1-fou8.onrender.com/api",
-  withCredentials: true, // 🔥 Required for cookies to be sent with request
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-backend1-fou8.onrender.com/api" : "/api",
+  withCredentials: true,
 });
