da340865e67ce4c5f3bc9ff4095aa3d7a95c9f8b
Priyanshucs22 <priyanshusharma0156@gmail.com>
2025-06-17 16:24:56 +0530

    frontend updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api" // or your local backend URL
+    : "https://chat-app-4ua6.onrender.com/api"; // your actual deployed backend
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-4ua6.onrender.com/api" : "/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
